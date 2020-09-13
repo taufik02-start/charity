b9f0cd577311d5880e7d7af70bb132012ce21392
Taufik <taufik1799hidayat@gmail.com>
2020-09-13 16:25:24 +0700

    penyusunan folder

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,3 @@
 <script>
-	import Navigation from './Navigation.svelte';
-
-	let title = "Taufik";
-	setTimeout(()=>{
-		title = "Rubah"
-	},2000);
-	let mahasiswa = ['taufik','diki'];
+	import Home from '.pages/home.svelte'
 </script>
-
-<div>
-	<h1>{title}</h1>
-	<Navigation mahasiswa={mahasiswa}/>
-</div>
-<style>
-	h1{
-		color:black;
-		size:20px;
-	}
-	:global(div){
-		border:'solid' 0.5em black;
-		padding: 1em;
-	}
-</style>
-<svelte:head>
-	<!-- <link rel="stylesheet" href="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css" /> -->
-</svelte:head>
---
+++ src/pages/home.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Navigation from '../components/Navigation.svelte';
+
+	let title = "Taufik";
+	let mahasiswa = ['taufik','diki'];
+</script>
+
+<div>
+	<h1>{title}</h1>
+	<Navigation mahasiswa={mahasiswa}/>
+</div>
+<style>
+	h1{
+		color:black;
+		size:20px;
+	}
+</style>
+<svelte:head>
+	<!-- <link rel="stylesheet" href="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css" /> -->
+</svelte:head>
